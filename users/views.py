c1c895d5b6ba980cd62a53488c07b2999fa9ba7c
Alexander Castille <C00406499@louisiana.edu>
2022-04-06 23:34:09 -0500

    trying to figure out query

--- users/views.py
+++ users/views.py
@@ -51,5 +51,5 @@ class BundleView(LoginRequiredMixin, generic.ListView):
         userBundles = Bundle.objects.filter(userID = self.request.user)
         context = super().get_context_data(**kwargs)
         context['bundles'] = userBundles
-        context['bundleReminders'] = Reminder.objects.filter(Reminder.bundleID)
+        context['bundleReminders'] = Reminder.objects.filter(bundleID=userBundles)
         return context
