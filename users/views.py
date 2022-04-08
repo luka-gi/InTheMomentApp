c310f4f4a26e3d609b6175a2fec0388505da4cdd
Chris Thibodeaux <c00468013@louisiana.edu>
2022-04-07 21:20:05 -0500

    new-commit

--- users/views.py
+++ users/views.py
@@ -29,18 +29,38 @@ class SignUpView(generic.CreateView):
 class HomeView(MapTemplateView):
     template_name = 'home.html'
 
-class CreateReminderView(generic.CreateView, MapTemplateView):
+class CreateReminderView(generic.CreateView):
 
     form_class = ReminderForm
     success_url = reverse_lazy('home')
     template_name = 'create_reminder.html'
 
+    def get_context_data(self, **kwargs):
+        userBundles = Bundle.objects.filter(userID = self.request.user)
+        context = super().get_context_data(**kwargs)
+        context['reminders'] = Reminder.objects.filter(bundleID__in=userBundles)
+        return context
+
     def form_valid(self, form):
         form.instance.bundleID = Bundle.objects.filter(name="Default").get(userID = self.request.user)
         return super().form_valid(form)
 
-class SettingsView(MapTemplateView):
+class SettingsView(generic.CreateView, MapTemplateView):
+    form_class = CustomUserCreationForm
     template_name="settings.html"
+    success_url=reverse_lazy('settings')
+
+    def form_valid(self, form):
+        Cur_user = self.request.user
+        if(form.instance.username != None):
+            user.username = form.instance.username
+        if(form.instance.email != None):
+            user.email = form.instance.email
+        if(form.instance.password != None):
+            user.password = form.instance.password
+
+        
+        return super().form_valid(form)
 
 class BundleView(MapTemplateView):
 
@@ -53,5 +73,5 @@ class BundleView(MapTemplateView):
         context['bundles'] = userBundles
         return context
 
-class CreateBundleView(generic.CreateView, MapTemplateView):
+class CreateBundleView(MapTemplateView):
     template_name="create_bundle.html"
